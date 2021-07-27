e25cfd7ce33f47c0875368cbe43b6d0c6e34c105
David Valderrama <dvalderrama08@gmail.com>
2021-07-26 22:56:22 -0700

    Added /api/workouts route

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
 
 const db = require("./models");
+const { Workout } = require("./models");
 
 const app = express();
 
@@ -16,6 +17,16 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+app.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+    .then(workout => {
+        res.json(workout);
+    })
+    .catch(err => {
+        res.json(err);
+    });
+});
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
